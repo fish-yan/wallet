646aa434ec02719881cba5f7d2743839c8a59516
yan <757095197@qq.com>
2023-04-17 11:26:13 +0800

    Initial Commit

---
+++ Wallet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wallet
+//
+//  Created by 薛焱 on 2023/4/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
